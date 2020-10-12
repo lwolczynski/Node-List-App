9e3de9086970219e28fd6c515ceb7fc60456c665
unknown <lwolczynski7@gmail.com>
2020-10-11 22:53:11 -0500

    [feat] Add filenames listing + information if it is file or folder

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const fs = require('fs');
 const process = require('process');
 
-fs.readdir(process.cwd(), (err, filenames) => {
+const lstat = fs.promises.lstat;
+
+fs.readdir(process.cwd(), async (err, filenames) => {
     if (err) {
         console.log(err);
     }
-    console.log(filenames);
+    
+    const stats = await Promise.all(filenames.map(filename => lstat(filename)));
+
+    for (let stat of stats) {
+        const index = stats.indexOf(stat);
+        console.log(filenames[index], stat.isFile());
+    }
 });
